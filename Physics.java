8280da671a1312d7fd5af147721e4ba019e97a90
Osetohanme <129763480+Osetohanme@users.noreply.github.com>
2024-05-25 22:51:52 +0100

    Delete Physics.java

--- Physics.java
+++
@@ -1,9 +0,0 @@
-public class Physics {
-    
-    float calculateVelocity(float distance, int time){
-        return distance/time;
-    }
-    float calculateVelocity(float initiaVelocity, float acceleration, int time){
-        return initiaVelocity + (acceleration * time);
-    }
-}
